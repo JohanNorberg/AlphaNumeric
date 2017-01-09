90fe90f5d0041aed56d8715b6fed82b7e3f1ad3c
Johan Norberg <johan.norberg.1987@gmail.com>
2017-01-09 18:19:27 +0100

    Nuget added and changed target framework to 4.

--- AlphaNumeric/Properties/AssemblyInfo.cs
+++ AlphaNumeric/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("AlphaNumeric")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Two ways of encoding data or string to only use alpha-numeric characters.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Jepono")]
 [assembly: AssemblyProduct("AlphaNumeric")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
@@ -17,7 +17,7 @@ using System.Runtime.InteropServices;
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
+[assembly: ComVisible(true)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("bbec233e-2a5e-4be3-898c-47eb64492afc")]
